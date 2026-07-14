de2ea2e3deb3714f17859a7318fb8f1409d7f34c
Li Peng <liepng@unisound.com>
2026-07-14 11:50:21 +0800

    style: format site metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,10 +15,11 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Li Peng\'s Running Page',
+  siteTitle: "Li Peng's Running Page",
   siteUrl: 'https://lipeng31.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'My running statistics. Thanks to [Yi Hong](https://github.com/yihong0618) for developing this amazing project.',
+  description:
+    'My running statistics. Thanks to [Yi Hong](https://github.com/yihong0618) for developing this amazing project.',
   navLinks: [
     {
       name: 'Summary',
